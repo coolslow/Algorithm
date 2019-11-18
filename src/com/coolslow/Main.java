9d12b059e9d9018487f0bba891618a79ac2cdc5d
mizhon <mizhon@live.cn>
2019-11-18 19:58:48 +0800

    update codes:

--- src/com/coolslow/Main.java
+++ src/com/coolslow/Main.java
@@ -3,7 +3,8 @@ package com.coolslow;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
         System.out.println("testing ...");
     }
 }
+
