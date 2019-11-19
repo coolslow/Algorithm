d881fcb77b706f25030ad3a8b11f38e28d1a5bf4
mizhon <mizhon@live.cn>
2019-11-19 17:11:16 +0800

    添加'两数之和'的暴力解法

--- src/com/coolslow/Main.java
+++ src/com/coolslow/Main.java
@@ -1,11 +1,15 @@
 package com.coolslow;
 
+import com.coolslow.leetcode.top100.SumTwoNum;
+
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
-        System.out.println("testing ...");
+        int[] nums = new int[] { 2, 7, 11, 15 };
+        int target = 9;
+        int[] res = SumTwoNum.sumTwoNum(nums, target);
+        System.out.println(res.length);
     }
-
 }
 
