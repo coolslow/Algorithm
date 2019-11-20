44ade0ba158cbf003b840d2e4f62007f7a0400a0
mizhon <mizhon@live.cn>
2019-11-20 19:47:46 +0800

    update test case

--- test/com/coolslow/RemoveDuplicateFromSortedArrayTest.java
+++ test/com/coolslow/RemoveDuplicateFromSortedArrayTest.java
@@ -7,9 +7,12 @@ import org.junit.Test;
 public class RemoveDuplicateFromSortedArrayTest {
     @Test
     public void shouldRemoveDuplicateFromSourtedArray() {
+        int [] emptyArr = new int[] {};
         int[] nums = new int[] {0, 0, 1, 1, 1, 2, 3, 3, 4, 5, 5};
 
         int result = RemoveDuplcateFromSortedArray.removeDuplicates(nums);
+        int result1 = RemoveDuplcateFromSortedArray.removeDuplicates(emptyArr);
         Assert.assertEquals(6, result);
+        Assert.assertEquals(0, result1);
     }
 }
