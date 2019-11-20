a28d23be57be7e6011d0c545694a29a99daff363
mizhon <mizhon@live.cn>
2019-11-20 14:48:41 +0800

    添加代码

--- src/com/coolslow/leetcode/top100/TwoSumPlayground.java
+++ src/com/coolslow/leetcode/top100/TwoSumPlayground.java
@@ -6,12 +6,30 @@ import java.util.Map;
 public class TwoSumPlayground {
 
     /**
-     * 使用HashMap一次遍历存储数组中的数据，再逐个比较
+     * 暴力解法，时间复杂度为O(n^2)
      * @param nums
      * @param target
      * @return
      */
     public static int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j= i + 1; j < nums.length; j++) {
+                if(nums[j] == target - nums[i]) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        
+        return new int[] {};
+    }
+    
+    /**
+     * 使用HashMap一次遍历存储数组中的数据，再逐个比较
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSumWithHashMap(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
             map.put(nums[i], i);
