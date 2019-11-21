0e84275a0c7caf275dfb1bf1c447beaf03ce42e9
mizhon <mizhon@live.cn>
2019-11-21 12:50:52 +0800

    playground里添加hashmap一次遍历算法

--- src/com/coolslow/leetcode/top100/TwoSumPlayground.java
+++ src/com/coolslow/leetcode/top100/TwoSumPlayground.java
@@ -44,4 +44,17 @@ public class TwoSumPlayground {
 
         return new int[] {};
     }
+    
+    public static int[] twoSumWithOnePassHashMap(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            int tmp = target - nums[i];
+            if(map.containsKey(tmp)) {
+                return new int[] {map.get(tmp), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
 }
