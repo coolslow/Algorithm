f7d2c6c9868a5ca3bcf3a59a91302044dffe7e62
mizhon <mizhon@live.cn>
2019-12-03 09:29:55 +0800

    复习数组去重算法

--- src/com/coolslow/leetcode/top100/playground/RemoveDuplicateFromSortedArrayPlayground.java
+++ src/com/coolslow/leetcode/top100/playground/RemoveDuplicateFromSortedArrayPlayground.java
@@ -17,4 +17,49 @@ public class RemoveDuplicateFromSortedArrayPlayground {
         }
         return count + 1;
     }
+
+    public static int removeDuplicateNumFromArray(int[] nums) {
+        if(nums.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[count] != nums[i]) {
+                count++;
+                nums[count] = nums[i];
+            }
+        }
+        return count + 1;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
