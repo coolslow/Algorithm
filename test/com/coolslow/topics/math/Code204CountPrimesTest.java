2b51bf984105143f47c1b3b4d023f9c5b2ec9040
Admin <mrthanksgiving@163.com>
2020-01-10 17:46:29 +0800

    leet code 204 计数质数

---
+++ test/com/coolslow/topics/math/Code204CountPrimesTest.java
@@ -0,0 +1,20 @@
+package com.coolslow.topics.math;
+
+import com.coolslow.datastruct.utils.MyUtils;
+import com.coolslow.leetcode.topics.math.Code204CountPrimes;
+import org.junit.Test;
+
+/**
+ * by MrThanksgiving
+ */
+public class Code204CountPrimesTest {
+
+    @Test
+    public void testSolution() {
+        Code204CountPrimes solution = new Code204CountPrimes();
+//        int n = 499979;
+        int n = 10;
+        int result = solution.countPrimes(n);
+        MyUtils.println(MyUtils.ANSI_RED + result + MyUtils.ANSI_RESET);
+    }
+}
