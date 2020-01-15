24427cf5da13c633bbc43afa925ae69c7bc579c9
mizhon <mizhon@live.cn>
2020-01-15 17:46:55 +0800

    update codes

---
+++ test/com/coolslow/top100plus/NO137_SingleNumberIITest.java
@@ -0,0 +1,16 @@
+package com.coolslow.top100plus;
+
+import com.coolslow.leetcode.top100plus.NO137_SingleNumberII;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NO137_SingleNumberIITest {
+
+    @Test
+    public void shouldSingleNumberIIGetCorrectNum() {
+
+        int[] arr = new int[] { 0, 1, 0, 1, 0, 1, 9};
+        int result = NO137_SingleNumberII.singleNumberII(arr);
+        Assert.assertEquals(9, result);
+    }
+}
