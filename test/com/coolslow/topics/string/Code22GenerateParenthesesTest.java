580ce13c3244eb6b367799f55700dcc554b4a6ea
Admin <mrthanksgiving@163.com>
2020-01-16 19:44:24 +0800

    LeetCode 22 生成括号

---
+++ test/com/coolslow/topics/string/Code22GenerateParenthesesTest.java
@@ -0,0 +1,21 @@
+package com.coolslow.topics.string;
+
+import com.coolslow.datastruct.utils.MyUtils;
+import com.coolslow.leetcode.topics.string.Code22GenerateParentheses;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * by MrThanksgiving
+ */
+public class Code22GenerateParenthesesTest {
+
+    @Test
+    public void testSolution() {
+        Code22GenerateParentheses solution = new Code22GenerateParentheses();
+        int input = 3;
+        List<String> result = solution.generateParenthesis(input);
+        MyUtils.println(MyUtils.ANSI_RED + result + MyUtils.ANSI_RESET);
+    }
+}
