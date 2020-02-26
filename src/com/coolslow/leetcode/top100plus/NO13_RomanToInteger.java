9c47f6345ebd2f7b385150dffbff5a8ae2f3a369
mizhon <mizhon@live.cn>
2020-02-26 15:49:06 +0800

    update codes

--- src/com/coolslow/leetcode/top100plus/NO13_RomanToInteger.java
+++ src/com/coolslow/leetcode/top100plus/NO13_RomanToInteger.java
@@ -1,5 +1,8 @@
 package com.coolslow.leetcode.top100plus;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 13. 罗马数字转整数
  *
@@ -92,7 +95,7 @@ public class NO13_RomanToInteger {
         }
     }
 
-    ///////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
 
     /**
      * 该方法执行速度更快
@@ -121,5 +124,36 @@ public class NO13_RomanToInteger {
         }
 
         return count;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    public static int romanToIntWithHashMap(String s) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("I", 1);
+        map.put("IV", 4);
+        map.put("V", 5);
+        map.put("IX", 9);
+        map.put("X", 10);
+        map.put("XL", 40);
+        map.put("L", 50);
+        map.put("XC", 90);
+        map.put("C", 100);
+        map.put("CD", 400);
+        map.put("D", 500);
+        map.put("CM", 900);
+        map.put("M", 1000);
+
+        int ans = 0;
+        for(int i = 0; i < s.length();) {
+            if(i + 1 < s.length() && map.containsKey(s.substring(i, i + 2))) {
+                ans += map.get(s.substring(i, i + 2));
+                i += 2;
+            } else {
+                ans += map.get(s.substring(i, i + 1));
+                i ++;
+            }
+        }
+
+        return ans;
     }
 }
