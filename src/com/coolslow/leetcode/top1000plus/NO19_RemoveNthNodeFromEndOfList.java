c7d5085d21af344cf5d2c8eb899807398d95143f
mizhon <mizhon@live.cn>
2020-03-25 17:22:01 +0800

    更新链表：删除倒数第N个几点的算法

--- src/com/coolslow/leetcode/top1000plus/NO19_RemoveNthNodeFromEndOfList.java
+++ src/com/coolslow/leetcode/top1000plus/NO19_RemoveNthNodeFromEndOfList.java
@@ -21,11 +21,11 @@ import com.coolslow.leetcode.top1000plus.datastructure.ListNode;
 public class NO19_RemoveNthNodeFromEndOfList {
 
     /**
-     * 两次遍历算法
+     * 方法一：两次遍历算法
      * 时间复杂度：O(L)，该算法对列表进行了两次遍历，首先计算了列表的长度 L 其次找到第 (L - n) 个结点。 操作执行了 2L-n 步，时间复杂度为 O(L)。
      *
      */
-    public ListNode removeNthNodeFromEndOfList(ListNode head, int n) {
+    public static ListNode removeNthNodeFromEndOfList(ListNode head, int n) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         int length = 0;
@@ -37,12 +37,44 @@ public class NO19_RemoveNthNodeFromEndOfList {
         }
         // 找到第(L-n)个节点的位置下标
         length -= n;
+        // 在上一次循环中，first已经指向了链表的尾部，那么first = dummy将重新指向链表头部。
         first = dummy;
         while (length > 0) {
             length--;
             first = first.next;
         }
+        // 删除倒数第n个位置上的节点
         first.next = first.next.next;
+
+        return dummy.next;
+    }
+
+    /**
+     * 方法二：一次遍历
+     *
+     * 采用双指针的方式，第一个指针从列表的开头向前移动 n + 1 步，而第二个指针从链表的开头出发。
+     * 此时，这两个指针的间距为n。同时移动两个指针，向前保持这种恒定间隔。直到第一个指针到达最后一个节点。
+     */
+    public static ListNode removeTheNthFromEndOfListNode(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        // 先让第一个指针向前移动 n 步。
+        for(int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+
+        // 再让两个指针同时先前移动
+        while(first != null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        // 删除倒数 n 位置的节点
+        second.next = second.next.next;
         return dummy.next;
     }
 }
