f63f4b79632ee7a6276e24c38046e8772b9f2404
mizhon <mizhon@live.cn>
2020-03-29 19:06:56 +0800

    添加判断链表有环面试题

--- src/com/coolslow/leetcode/top1000plus/NO141_LinkListCycle.java
+++ src/com/coolslow/leetcode/top1000plus/NO141_LinkListCycle.java
@@ -19,8 +19,10 @@ public class NO141_LinkListCycle {
      * 解法一：哈希表
      *
      * 思路：我们可以通过检查一个结点此前是否被访问过来判断链表是否为环形链表。常用的方法是使用哈希表。
+     * - 时间复杂度O(n)，对于含有n个元素的链表，访问每个元素最多一次，添加一个节点到哈希表只需花费O(1)的时间。
+     * - 空间复杂度O(n)，空间取决于添加到哈希表中的元素数目，最多可添加n个元素。
      */
-    public boolean hasCycle(ListNode head) {
+    public boolean hasCycleWithHashSet(ListNode head) {
         Set<ListNode> nodesSeen = new HashSet<>();
         while (head != null) {
             if (nodesSeen.contains(head)) {
@@ -31,5 +33,26 @@ public class NO141_LinkListCycle {
             head = head.next;
         }
         return false;
+    }
+
+    /**
+     * 解法二：双指针
+     *
+     *
+     */
+    public boolean hasCycleWithTwoPointers(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
     }
 }
