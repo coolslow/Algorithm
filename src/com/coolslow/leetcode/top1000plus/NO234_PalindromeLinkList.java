b07ecf2aa61f6da0aa32e6ca4f10a05ee8cf1816
mizhon <mizhon@live.cn>
2020-04-01 17:44:04 +0800

    update codes

---
+++ src/com/coolslow/leetcode/top1000plus/NO234_PalindromeLinkList.java
@@ -0,0 +1,21 @@
+package com.coolslow.leetcode.top1000plus;
+
+/**
+ * 234. 回文链表
+ *
+ * 题目：请判断一个链表是否为回文链表。
+ *
+ * 示例 1:
+ *      输入: 1->2
+ *      输出: false
+ *
+ * 示例 2:
+ *      输入: 1->2->2->1
+ *      输出: true
+ *
+ */
+
+@Deprecated
+public class NO234_PalindromeLinkList {
+
+}
