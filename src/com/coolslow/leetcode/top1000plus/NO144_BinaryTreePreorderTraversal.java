7ef3e52fd77e102f19ad7bd066e8b975bc0ac1e0
mizhon <mizhon@live.cn>
2020-04-17 17:20:24 +0800

    添加二叉树的前序遍历算法题

---
+++ src/com/coolslow/leetcode/top1000plus/NO144_BinaryTreePreorderTraversal.java
@@ -0,0 +1,18 @@
+package com.coolslow.leetcode.top1000plus;
+
+/**
+ * 144. 二叉树的前序遍历
+ *
+ * 给定一个二叉树，返回他的前序遍历的结果。
+ *
+ * 示例：
+ *
+ * 输入：[1, null, 2, 3]
+ *       1
+ *        \
+ *         2
+ *        /
+ *       3
+ */
+public class NO144_BinaryTreePreorderTraversal {
+}
