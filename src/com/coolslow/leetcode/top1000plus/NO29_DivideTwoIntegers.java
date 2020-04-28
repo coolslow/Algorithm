34f530bdb209da7e934552c05892e95bc30c07c8
mizhon <mizhon@live.cn>
2020-04-28 22:02:11 +0800

    添加两数相除的解法

--- src/com/coolslow/leetcode/top1000plus/NO29_DivideTwoIntegers.java
+++ src/com/coolslow/leetcode/top1000plus/NO29_DivideTwoIntegers.java
@@ -25,4 +25,39 @@ package com.coolslow.leetcode.top1000plus;
  *   - 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−2^31,  2^31 − 1]。本题中，如果除法结果溢出，则返回 2^31 − 1。
  */
 public class NO29_DivideTwoIntegers {
+
+    public static int divide(int dividend, int divisor) {
+        boolean sign = (dividend > 0) ^ (divisor > 0);
+        int result = 0;
+
+        if(dividend > 0) {
+            dividend = -dividend;
+        }
+
+        if(divisor > 0) {
+            divisor = -divisor;
+        }
+
+        while(dividend <= divisor) {
+            int temp_result = -1;
+            int temp_divisor = divisor;
+
+            while(dividend <= (temp_divisor << 1)) {
+                if(temp_divisor <= (Integer.MIN_VALUE >> 1)){
+                    break;
+                }
+                temp_result = temp_result << 1;
+                temp_divisor = temp_divisor << 1;
+            }
+            dividend = dividend - temp_divisor;
+            result += temp_result;
+        }
+        if(!sign) {
+            if(result <= Integer.MIN_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            result = - result;
+        }
+        return result;
+    }
 }
