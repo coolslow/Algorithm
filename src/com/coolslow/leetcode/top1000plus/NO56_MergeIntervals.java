c5cfa3d10ffa2ff3a34abb5eaa7353614e39ef52
mizhon <mizhon@live.cn>
2020-05-03 19:33:11 +0800

    update codes, add comments

--- src/com/coolslow/leetcode/top1000plus/NO56_MergeIntervals.java
+++ src/com/coolslow/leetcode/top1000plus/NO56_MergeIntervals.java
@@ -1,7 +1,9 @@
 package com.coolslow.leetcode.top1000plus;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 /**
  * 56. 合并区间
@@ -20,22 +22,29 @@ import java.util.Comparator;
  */
 public class NO56_MergeIntervals {
     public static int[][] merge(int[][] intervals) {
+        int len = intervals.length;
+        if (len < 2) {
+            return intervals;
+        }
+
         // 先按照区间起始位置排序
         Arrays.sort(intervals, Comparator.comparingInt(v -> v[0]));
-        // 遍历区间
-        int[][] res = new int[intervals.length][2];
-        int idx = -1;
 
-        for (int[] interval: intervals) {
-            // 如果结果数组是空的，或者当前区间的起始位置 > 结果数组中最后区间的终止位置，
-            // 则不合并，直接将当前区间加入结果数组。
-            if (idx == -1 || interval[0] > res[idx][1]) {
-                res[++idx] = interval;
+        // 也可以使用 Stack，因为我们只关心结果集的最后一个区间
+        List<int[]> res = new ArrayList<>();
+        res.add(intervals[0]);
+
+        for (int i = 1; i < len; i++) {
+            int[] curInterval = intervals[i];
+            // 每次新遍历到的列表与当前结果集中的最后一个区间的末尾端点进行比较
+            int[] peek = res.get(res.size() - 1);
+            if (curInterval[0] > peek[1]) {
+                res.add(curInterval);
             } else {
-                // 反之将当前区间合并至结果数组的最后区间
-                res[idx][1] = Math.max(res[idx][1], interval[1]);
+                // 注意，这里应该取最大
+                peek[1] = Math.max(curInterval[1], peek[1]);
             }
         }
-        return Arrays.copyOf(res, idx + 1);
+        return res.toArray(new int[res.size()][]);
     }
 }
