ed50559f2bc866819ae08a5a0aa90c8bd3c0b1a2
mizhon <mizhon@live.cn>
2020-05-06 11:30:30 +0800

    添加字符串反转试题

---
+++ src/com/coolslow/leetcode/top1000plus/NO557_ReverseWordsInStringIII.java
@@ -0,0 +1,30 @@
+package com.coolslow.leetcode.top1000plus;
+
+/**
+ * 557. 反转字符串中的单词 III
+ * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+ *
+ * 示例 1:
+ *  输入: "Let's take LeetCode contest"
+ *  输出: "s'teL ekat edoCteeL tsetnoc"
+ *
+ * 注意：在字符串中，每个单词由单个空格分隔，并且字符串中不会有任何额外的空格。
+ */
+public class NO557_ReverseWordsInStringIII {
+
+    /**
+     * 时间复杂度：O(n) 。其中 n 是字符串的长度。
+     * 空间复杂度：O(n) 。使用了大小为 n 的 res。
+     *
+     * @param s 给定的单词
+     * @return 返回反转后的字符串
+     */
+    public static String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder res=new StringBuilder();
+        for (String word: words) {
+            res.append(new StringBuffer(word).reverse().toString()).append(" ");
+        }
+        return res.toString().trim();
+    }
+}
