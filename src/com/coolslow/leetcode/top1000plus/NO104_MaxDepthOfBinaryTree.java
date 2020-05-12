ee8ac5939c6fa3237e6cf15e707e9c96929c7fc6
mizhon <mizhon@live.cn>
2020-05-12 12:06:22 +0800

    update codes

--- src/com/coolslow/leetcode/top1000plus/NO104_MaxDepthOfBinaryTree.java
+++ src/com/coolslow/leetcode/top1000plus/NO104_MaxDepthOfBinaryTree.java
@@ -20,6 +20,18 @@ import com.coolslow.leetcode.top1000plus.datastructure.Tree.BinaryTree.TreeNode;
  */
 public class NO104_MaxDepthOfBinaryTree {
 
+    /**
+     * 解法一：递归求解
+     *
+     * 深度有限的策略 DFS
+     * 时间复杂度：每个节点只访问一次，因此时间复杂度O(N), N为节点的数量
+     * 空间复杂度：
+     *  - 最糟糕的情况下，树是全部不平衡二叉树。树向一侧倾斜，此时的时间复杂度为O(N)
+     *  - 最好的情况，树是一个完全平衡二叉树，此时时间复杂度为O(log(N))
+     *
+     * @param root 给定的二叉树
+     * @return 返回二叉树的最大深度
+     */
     public static int maxDepth(TreeNode<Integer> root) {
         if(root == null) {
             return 0;
