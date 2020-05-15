433114e9e248557ee560733f8882e5c99864c691
mizhon <mizhon@live.cn>
2020-05-15 12:58:59 +0800

    移除不必要引用

--- src/com/coolslow/leetcode/top1000plus/NO17_LetterCombinationsOfaPhoneNumber.java
+++ src/com/coolslow/leetcode/top1000plus/NO17_LetterCombinationsOfaPhoneNumber.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static javax.swing.UIManager.put;
-
 /**
  * 17. 电话号码的字母组合
  * 题目：给定一个仅包含数字 2 ～ 9 的字符串，返回所有它能表示的字母组合。
