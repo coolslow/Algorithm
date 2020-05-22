2ecf8a7ddac0b80ada2b30737aa2b32cd1e40327
mizhon <mizhon@live.cn>
2020-05-22 23:41:51 +0800

    update codes

--- src/com/coolslow/leetcode/top1000plus/NO43_MultiplyStrings.java
+++ src/com/coolslow/leetcode/top1000plus/NO43_MultiplyStrings.java
@@ -21,57 +21,54 @@ package com.coolslow.leetcode.top1000plus;
  */
 public class NO43_MultiplyStrings {
 
-//    /**
-//     * 思路：因为num1和num2可能非常长，因此不能直接转为整型运算（题目也不允许）因此只能模拟手动乘法的办法
-//     *
-//     * NOTE: 该解法无法通过测试拥立
-//     */
-//    public static String multiply(String num1, String num2) {
-//
-//        if(num1.equals("0") && num2.equals("0")) {
-//            return "0";
-//        }
-//        String res = "";
-//        for(int i = num2.length() - 1; i >= 0; i--) {
-//            int carry = 0;
-//            // 保存 num2 第i位数字与 num1 相乘的结果
-//            StringBuilder temp = new StringBuilder();
-//            for(int j = 0; j < num2.length() - 1 - i; j++) {
-//                temp.append(0);
-//            }
-//            int n2 = num2.charAt(i) - '0';
-//
-//            // num2 的第 i 位数字 n2 与 num1 相乘
-//            for(int j = num1.length() - 1; j >= 0 || carry != 0; j--) {
-//                int n1 = j < 0 ? 0 : num1.charAt(j) - '0';
-//                int product = (n1 * n2 + carry) % 10;
-//                temp.append(product);
-//                carry = (n1 * n2 + carry) / 10;
-//            }
-//            // 将当前结果与新计算的结果求和作为新的结果
-//            res = addStrings(res, temp.reverse().toString());
-//        }
-//
-//        return res;
-//    }
-//
-//    /**
-//     * 对两个字符串数字进行相加，返回字符串形式的和
-//     */
-//    public static String addStrings(String num1, String num2) {
-//        StringBuilder builder = new StringBuilder();
-//        int carry = 0;
-//        for (int i = num1.length() - 1, j = num2.length() - 1;
-//             i >= 0 || j >= 0 || carry != 0;
-//             i--, j--) {
-//            int x = i < 0 ? 0 : num1.charAt(i) - '0';
-//            int y = j < 0 ? 0 : num2.charAt(j) - '0';
-//            int sum = (x + y + carry) % 10;
-//            builder.append(sum);
-//            carry = (x + y + carry) / 10;
-//        }
-//        return builder.reverse().toString();
-//    }
+    public static String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        // 保存计算结果
+        String res = "0";
+
+        // num2 逐位与 num1 相乘
+        for (int i = num2.length() - 1; i >= 0; i--) {
+            int carry = 0;
+            // 保存 num2 第i位数字与 num1 相乘的结果
+            StringBuilder temp = new StringBuilder();
+            // 补 0
+            for (int j = 0; j < num2.length() - 1 - i; j++) {
+                temp.append(0);
+            }
+            int n2 = num2.charAt(i) - '0';
+
+            // num2 的第 i 位数字 n2 与 num1 相乘
+            for (int j = num1.length() - 1; j >= 0 || carry != 0; j--) {
+                int n1 = j < 0 ? 0 : num1.charAt(j) - '0';
+                int product = (n1 * n2 + carry) % 10;
+                temp.append(product);
+                carry = (n1 * n2 + carry) / 10;
+            }
+            // 将当前结果与新计算的结果求和作为新的结果
+            res = addStrings(res, temp.reverse().toString());
+        }
+        return res;
+    }
+
+    /**
+     * 对两个字符串数字进行相加，返回字符串形式的和
+     */
+    private static String addStrings(String num1, String num2) {
+        StringBuilder builder = new StringBuilder();
+        int carry = 0;
+        for (int i = num1.length() - 1, j = num2.length() - 1;
+             i >= 0 || j >= 0 || carry != 0;
+             i--, j--) {
+            int x = i < 0 ? 0 : num1.charAt(i) - '0';
+            int y = j < 0 ? 0 : num2.charAt(j) - '0';
+            int sum = (x + y + carry) % 10;
+            builder.append(sum);
+            carry = (x + y + carry) / 10;
+        }
+        return builder.reverse().toString();
+    }
 
     /**
      * 解法二：优化竖式
