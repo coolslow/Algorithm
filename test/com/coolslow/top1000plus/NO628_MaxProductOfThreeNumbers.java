4d88344e1ffffee769c49453af3cae3c4be0a684
mizhon <mizhon@live.cn>
2020-05-23 20:22:40 +0800

    添加三个数的最大乘积面试题

---
+++ test/com/coolslow/top1000plus/NO628_MaxProductOfThreeNumbers.java
@@ -0,0 +1,35 @@
+package com.coolslow.top1000plus;
+
+import java.util.Arrays;
+
+/**
+ * 628. 三个数的最大乘积
+ *
+ * 给定一个整型数组，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+ *
+ * 示例 1:
+ * 输入: [1,2,3]
+ * 输出: 6
+ *
+ * 示例 2:
+ * 输入: [1,2,3,4]
+ * 输出: 24
+ *
+ * 注：输入的数组中任意三个数的乘积不会超出32位有符号整数的范围。
+ */
+public class NO628_MaxProductOfThreeNumbers {
+    /**
+     * 方法一：排序
+     *
+     * 我们将数组进行升序排序，如果数组中所有的元素都是非负数，那么答案即为最后三个元素的乘积。
+     * 如果数组中出现了负数，那么我们还需要考虑乘积中包含负数的情况，显然选择最小的两个负数和最大的一个正数是最优的，即为前两个元素与最后一个元素的乘积。
+     * 上述两个结果中的较大值就是答案。注意我们可以不用判断数组中到底有没有正数，0 或者负数，因为上述两个结果实际上已经包含了所有情况，最大值一定在其中。
+     *
+     * 时间复杂度：O(NlogN)，其中 N 是数组的长度。
+     * 空间复杂度：O(logN)，为排序使用的空间。
+     */
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        return Math.max(nums[0] * nums[1] * nums[nums.length - 1], nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3]);
+    }
+}
