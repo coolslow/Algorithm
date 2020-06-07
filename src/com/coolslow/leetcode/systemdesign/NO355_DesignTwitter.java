958a6bf0f3457c1348cf744806afb9307aa1b461
mizhon <mizhon@live.cn>
2020-06-07 16:15:24 +0800

    update codes for system design

--- src/com/coolslow/leetcode/systemdesign/NO355_DesignTwitter.java
+++ src/com/coolslow/leetcode/systemdesign/NO355_DesignTwitter.java
@@ -1,8 +1,6 @@
 package com.coolslow.leetcode.systemdesign;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * TAG: 系统设计
@@ -43,22 +41,50 @@ import java.util.Map;
  *
  */
 public class NO355_DesignTwitter {
+    // 推文类，是一个单链表
     private static class Tweet {
+        // 推文ID
+        private int id;
+        // 发推时间戳
+        private int timestamp;
+        private Tweet next;
 
+        // Tweet的构造函数
+        public Tweet(int id, int timestamp) {
+            this.id = id;
+            this.timestamp = timestamp;
+        }
     }
 
     public static class Twitter {
 
+        // 用户 id 和推文（单链表）的对应关系
         private Map<Integer, Tweet> twitter;
+        // 用户 id 和他关注的用户列表的对应关系
+        private Map<Integer, Set<Integer>> followings;
+        // 全局使用的时间戳，用户每发布一条推文 + 1
+        private static int timestamp = 0;
 
+        private static PriorityQueue<Tweet> maxHeap;
         /** Initialize your data structure here. */
         public Twitter() {
-
+            followings = new HashMap<>();
+            twitter = new HashMap<>();
+            maxHeap = new PriorityQueue<>((o1, o2) -> -o1.timestamp + o2.timestamp);
         }
 
         /** Compose a new tweet. */
         public void postTweet(int userId, int tweetId) {
+            timestamp++;
+            if(twitter.containsKey(userId)) {
+                Tweet oldHead = twitter.get(userId);
+                Tweet newHead = new Tweet(tweetId, timestamp);
+                newHead.next = oldHead;
 
+                twitter.put(userId, newHead);
+            } else {
+                twitter.put(userId, new Tweet(tweetId, timestamp));
+            }
         }
 
         /**
@@ -67,19 +93,73 @@ public class NO355_DesignTwitter {
          * Tweets must be ordered from most recent to least recent.
          */
         public List<Integer> getNewsFeed(int userId) {
-            List<Integer> feeds = new ArrayList<>();
+
+            // 由于是全局使用的，使用之前需要清空
+            maxHeap.clear();
+
+            // 如果自己发了推文也要算上
+            if (twitter.containsKey(userId)) {
+                maxHeap.offer(twitter.get(userId));
+            }
+
+            Set<Integer> followingList = followings.get(userId);
+            if(followingList != null && followingList.size()> 0) {
+                for (int followingId : followingList) {
+                    Tweet tweet = twitter.get(followingId);
+                    if (tweet != null) {
+                        maxHeap.offer(tweet);
+                    }
+                }
+            }
+
+            List<Integer> feeds = new ArrayList<>(10);
+            int count = 0;
+            while (!maxHeap.isEmpty() && count < 10){
+                Tweet head = maxHeap.poll();
+                feeds.add(head.id);
+                if (head.next != null) {
+                    maxHeap.offer(head.next);
+                }
+                count++;
+            }
 
             return feeds;
         }
 
         /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
         public void follow(int followerId, int followeeId) {
+            if (followerId == followeeId) {
+                return;
+            }
 
+            Set<Integer> followingList = followings.get(followerId);
+            if (followingList == null) {
+                Set<Integer> followeeSet = new HashSet<>();
+                followeeSet.add(followeeId);
+                followings.put(followerId, followeeSet);
+            } else {
+                if (followingList.contains(followeeId)) {
+                    return;
+                }
+                followingList.add(followeeId);
+            }
         }
 
         /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
         public void unfollow(int followerId, int followeeId) {
+            if (followeeId == followerId) {
+                return;
+            }
 
+            // 获取我自己的关注列表
+            Set<Integer> followingList = followings.get(followerId);
+
+            if (followingList == null) {
+                return;
+            }
+
+            // 这里删除之前无需做判断，因为查找是否存在以后，就可以删除，反正删除之前都要查找
+            followingList.remove(followeeId);
         }
     }
 }
