df86fef36d1b4c753bcdaef3d29999d6cb28efb6
mizhon <mizhon@live.cn>
2020-07-01 16:30:31 +0800

    添加全排列试题回溯解法

--- src/com/coolslow/leetcode/top1000plus/NO46_Permutations.java
+++ src/com/coolslow/leetcode/top1000plus/NO46_Permutations.java
@@ -1,5 +1,10 @@
 package com.coolslow.leetcode.top1000plus;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 46. 全排列
  * 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
@@ -18,4 +23,31 @@ package com.coolslow.leetcode.top1000plus;
  *
  */
 public class NO46_Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new LinkedList();
+
+        ArrayList<Integer> output = new ArrayList<Integer>();
+        for (int num : nums)
+            output.add(num);
+
+        int n = nums.length;
+        backtrack(n, output, res, 0);
+        return res;
+    }
+
+    private void backtrack(int n, ArrayList<Integer> output, List<List<Integer>> res, int first) {
+        // 所有数都填完了
+        if (first == n) {
+            res.add(new ArrayList<Integer>(output));
+        }
+
+        for (int i = first; i < n; i++) {
+            // 动态维护数组
+            Collections.swap(output, first, i);
+            // 继续递归填下一个数
+            backtrack(n, output, res, first + 1);
+            // 撤销操作
+            Collections.swap(output, first, i);
+        }
+    }
 }
