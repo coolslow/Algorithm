ddbdf0e8e5b5c2bd27eeeb1c9ed3369cc8a80d27
mizhon <mizhon@live.cn>
2020-07-03 16:18:21 +0800

    添加回溯的第二种方法

--- src/com/coolslow/leetcode/top1000plus/NO46_Permutations.java
+++ src/com/coolslow/leetcode/top1000plus/NO46_Permutations.java
@@ -23,10 +23,10 @@ import java.util.List;
  *
  */
 public class NO46_Permutations {
-    public List<List<Integer>> permute(int[] nums) {
+    public static List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new LinkedList();
 
-        ArrayList<Integer> output = new ArrayList<Integer>();
+        ArrayList<Integer> output = new ArrayList<>();
         for (int num : nums) {
             output.add(num);
         }
@@ -36,7 +36,7 @@ public class NO46_Permutations {
         return res;
     }
 
-    private void backtrack(int n, ArrayList<Integer> output, List<List<Integer>> res, int first) {
+    private static void backtrack(int n, ArrayList<Integer> output, List<List<Integer>> res, int first) {
         // 所有数都填完了
         if (first == n) {
             res.add(new ArrayList<Integer>(output));
@@ -49,6 +49,47 @@ public class NO46_Permutations {
             backtrack(n, output, res, first + 1);
             // 撤销操作
             Collections.swap(output, first, i);
+        }
+    }
+
+    /**
+     * 参考：https://leetcode-cn.com/problems/permutations/solution/hui-su-suan-fa-python-dai-ma-java-dai-ma-by-liweiw/
+     *
+     * @param nums 给定的数组
+     * @return 返回的所有结果集
+     */
+    public static List<List<Integer>> permuteII(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+
+        if(len == 0) {
+            return res;
+        }
+
+        Boolean[] used = new Boolean[len];
+        List<Integer> path = new ArrayList<>();
+
+        dfs(nums, len, 0, path, used, res);
+        return res;
+    }
+
+    private static void dfs(int[] nums, int len, int depth, List<Integer> path, Boolean[] used, List<List<Integer>> res) {
+        if(depth == len) {
+            // 特别注意的地方如下：
+            // res.add(path);
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for(int i = 0; i < len; i++) {
+            if(!used[i]) {
+                path.add(nums[i]);
+                used[i] = true;
+
+                dfs(nums, len, depth + 1, path, used, res);
+                used[i] = false;
+                path.remove(path.size() - 1);
+            }
         }
     }
 }
