15c4f1289371dca76bb80e2c6460d34e23325ac8
mizhon <mizhon@live.cn>
2020-09-11 11:44:57 +0800

    添加判断二叉树是否对称试题

---
+++ src/com/coolslow/leetcode/top1000plus/NO101_SymmetricBinaryTree.java
@@ -0,0 +1,46 @@
+package com.coolslow.leetcode.top1000plus;
+
+import com.coolslow.leetcode.top1000plus.datastructure.TreeNode;
+import com.sun.source.tree.Tree;
+
+/**
+ * Title: NO101_SymmetricBinaryTree
+ * Desc: 对称二叉树
+ * Date: 2020/9/11 10:39 上午
+ * Author: ming
+ *
+ * 题目：给定一个二叉树，检查它是否是镜像对称的。
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ */
+public class NO101_SymmetricBinaryTree {
+
+    public static boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    private static boolean isMirror(TreeNode p, TreeNode q) {
+        if(p == null && q == null) {
+            return true;
+        }
+
+        if(p == null || q == null) {
+            return false;
+        }
+
+        return p.val == q.val && isMirror(p.left, q.right) && isMirror(p.right, q.left);
+    }
+}
--- src/com/coolslow/leetcode/top1000plus/NO144_BinaryTreePreorderTraversal.java
+++ src/com/coolslow/leetcode/top1000plus/NO144_BinaryTreePreorderTraversal.java
@@ -1,5 +1,9 @@
 package com.coolslow.leetcode.top1000plus;
 
+import com.coolslow.leetcode.top1000plus.datastructure.TreeNode;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 144. 二叉树的前序遍历
  *
@@ -15,6 +19,32 @@ package com.coolslow.leetcode.top1000plus;
  *       3
  */
 
-@Deprecated
 public class NO144_BinaryTreePreorderTraversal {
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+
+        if(root == null) {
+            return output;
+        }
+
+        // 将根结点压入栈中
+        stack.add(root);
+        /* 必须保证：先压右孩子，再压左孩子 */
+        while(!stack.isEmpty()) {
+            // pollLast方法返回列表的最后一个元素（同时也将该元素从列表中移除）
+            TreeNode node = stack.pollLast();
+            output.add(node.val);
+
+            if(node.right != null) {
+                stack.add(node.left);
+            }
+
+            if(node.left != null) {
+                stack.add(node.right);
+            }
+        }
+        return output;
+    }
 }
