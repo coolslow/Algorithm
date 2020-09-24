79f46ae4cb00313194c18291b61beb4d6b28c456
mizhon <mizhon@live.cn>
2020-09-24 23:45:46 +0800

    添加路径总和试题及递归解法

--- src/com/coolslow/Main.java
+++ src/com/coolslow/Main.java
@@ -8,32 +8,32 @@ public class Main {
         String greeting = "Hello World Algorithm";
         System.out.println(greeting);
 
-        String s = "(()))";
-        isValid(s);
+//        String s = "(()))";
+//        isValid(s);
 
     }
 
-    public static boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < s.length(); i++) {
+//    public static boolean isValid(String s) {
+//        Stack<Character> stack = new Stack<>();
+//        for(int i = 0; i < s.length(); i++) {
+////            if(s.charAt(i) == '(') {
+////                stack.push('(');
+////            } else if(!stack.empty() && stack.peek() == '(') {
+////                stack.pop();
+////            } else {
+////                return false;
+////            }
 //            if(s.charAt(i) == '(') {
-//                stack.push('(');
-//            } else if(!stack.empty() && stack.peek() == '(') {
+//                stack.push(s.charAt(i));
+//            }
+//            else if(s.charAt(i) == ')' && !stack.empty()) {
 //                stack.pop();
 //            } else {
 //                return false;
 //            }
-            if(s.charAt(i) == '(') {
-                stack.push(s.charAt(i));
-            }
-            else if(s.charAt(i) == ')' && !stack.empty()) {
-                stack.pop();
-            } else {
-                return false;
-            }
-        }
-
-        return stack.empty();
-    }
+//        }
+//
+//        return stack.empty();
+//    }
 }
 
