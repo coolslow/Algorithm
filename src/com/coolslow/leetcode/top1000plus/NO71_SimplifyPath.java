e6f401e79b8fe61423d035ca638353d84e396719
mizhon <mizhon@live.cn>
2020-10-24 23:01:38 +0800

    添加试题-71，简化路径

--- src/com/coolslow/leetcode/top1000plus/NO71_SimplifyPath.java
+++ src/com/coolslow/leetcode/top1000plus/NO71_SimplifyPath.java
@@ -1,5 +1,11 @@
 package com.coolslow.leetcode.top1000plus;
 
+import com.sun.tools.javac.util.Context;
+import sun.net.ProgressSource;
+
+import javax.swing.plaf.nimbus.State;
+import java.util.Stack;
+
 /**
  * 71. 简化路径
  *
@@ -39,4 +45,23 @@ package com.coolslow.leetcode.top1000plus;
  *
  */
 public class NO71_SimplifyPath {
+    public static String simplifyPath(String path) {
+        String[] s = path.split("/");
+        Stack<String> stack = new Stack<>();
+
+        for (int i = 0; i < s.length; i++) {
+            if (!stack.isEmpty() && s[i].equals(".."))
+                stack.pop();
+            else if (!s[i].equals("") && !s[i].equals(".") && !s[i].equals(".."))
+                stack.push(s[i]);
+        }
+        if (stack.isEmpty())
+            return "/";
+
+        StringBuffer res = new StringBuffer();
+        for (int i = 0; i < stack.size(); i++) {
+            res.append("/" + stack.get(i));
+        }
+        return res.toString();
+    }
 }
