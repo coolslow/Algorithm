07e9a04da948b1760f9578f7732b71304e0d2f29
mizhon <mizhon@live.cn>
2020-12-21 23:40:00 +0800

    新增迭代方式解法

--- src/com/coolslow/leetcode/from_scratch/Tree/NO589_NAryTreePreorderTraversal.java
+++ src/com/coolslow/leetcode/from_scratch/Tree/NO589_NAryTreePreorderTraversal.java
@@ -1,5 +1,7 @@
 package com.coolslow.leetcode.from_scratch.Tree;
 
+import java.util.*;
+
 /**
  * 589. N叉树的前序遍历
  *
@@ -15,5 +17,44 @@ package com.coolslow.leetcode.from_scratch.Tree;
  *
  * NOTE: 前序遍历既：根 -> 左 -> 右
  */
+
 public class NO589_NAryTreePreorderTraversal {
+
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public static List<Integer> preorder(Node root) {
+
+        LinkedList<Node> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+
+        if (root == null) {
+            return output;
+        }
+        stack.add(root);
+
+        while (!stack.isEmpty()) {
+            Node node = stack.pollLast();
+            output.add(node.val);
+            Collections.reverse(node.children);
+            for(Node item: node.children) {
+                stack.add(item);
+            }
+        }
+        return output;
+    }
 }
