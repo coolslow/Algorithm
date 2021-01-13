4fa543d0ec52a8c99ab44204685e897ac873ab8f
mizhon <mizhon@live.cn>
2021-01-13 10:51:01 +0800

    添加105题解法

--- src/com/coolslow/leetcode/top1000plus/NO105_ConstructBinaryTreeFromPreorderAndInorderTraversal.java
+++ src/com/coolslow/leetcode/top1000plus/NO105_ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -2,6 +2,8 @@ package com.coolslow.leetcode.top1000plus;
 
 import com.coolslow.leetcode.top1000plus.datastructure.TreeNode;
 
+import java.util.HashMap;
+
 /**
  * 105. 从前序与中序遍历序列构造二叉树
  *
@@ -52,6 +54,35 @@ public class NO105_ConstructBinaryTreeFromPreorderAndInorderTraversal {
         root.left = buildTreeHelper(preorder, p_start + 1, p_start + leftNum + 1, inorder, i_start, i_root_index);
         // 递归构造右子树
         root.right = buildTreeHelper(preorder, p_start + leftNum + 1, p_end, inorder, i_root_index + 1, i_end);
+        return root;
+    }
+
+    /**
+     * 第一种方法存在一个问题，中序遍历中找到的根节点的位置每次都需要遍历中序遍历数组去查找。
+     * 可以利用HashMap把中序遍历数组中的每个元素的值和下标保存起来，这样，寻找根节点的位置能直接得到。
+     * @param preorder
+     * @param inorder
+     */
+    public static TreeNode buildTreeII(int[] preorder, int[] inorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return buildTreeHelperII(preorder, 0, preorder.length, inorder, 0, inorder.length, map);
+    }
+
+    private static TreeNode buildTreeHelperII(int[] preorder, int p_start, int p_end, int[] inorder, int i_start, int i_end, HashMap<Integer, Integer> map) {
+        if (p_start == p_end) {
+            return null;
+        }
+
+        int root_val = preorder[p_start];
+        TreeNode root = new TreeNode(root_val);
+
+        int i_root_index = map.get(root_val);
+        int leftNum = i_root_index - i_start;
+        root.left = buildTreeHelperII(preorder, p_start + 1, p_start + leftNum + 1, inorder, i_start, i_root_index, map);
+        root.right = buildTreeHelperII(preorder, p_start + leftNum + 1, p_end, inorder, i_root_index + 1, i_end, map);
         return root;
     }
 }
