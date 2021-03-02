2f8d0616ff976ea533a669502e1847b71adc94ea
mizhon <mizhon@live.cn>
2021-03-02 23:28:09 +0800

    添加求二叉树最大深度试题

--- src/com/coolslow/playground/binary_tree/MergeTwoBinaryTree.java
+++ src/com/coolslow/playground/binary_tree/MergeTwoBinaryTree.java
@@ -24,22 +24,6 @@ package com.coolslow.playground.binary_tree;
  * 	 5   4   7
  */
 public class MergeTwoBinaryTree {
-    // 定义二叉树的数据结构
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) {
-            this.val = val;
-        }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
 
         if (root1 == null) {
---
+++ src/com/coolslow/playground/binary_tree/TreeNode.java
@@ -0,0 +1,20 @@
+package com.coolslow.playground.binary_tree;
+
+/**
+ * 定义二叉树的数据结构
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
