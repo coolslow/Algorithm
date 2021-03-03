082d6e815ac4277f86e6bb241db89c72debabac1
mizhon <mizhon@live.cn>
2021-03-03 23:50:07 +0800

    添加814试题：二叉树剪枝

---
+++ src/com/coolslow/playground/binary_tree/NO814_BinaryTreePruning.java
@@ -0,0 +1,14 @@
+package com.coolslow.playground.binary_tree;
+
+/**
+ * 814. 二叉树剪枝
+ * 给定二叉树根结点 root ，此外树的每个结点的值要么是 0，要么是 1。
+ * 返回移除了所有不包含 1 的子树的原二叉树。
+ *
+ * 说明：
+ * 给定的二叉树最多有100个节点。
+ * 每个节点的值只会为0 或 1。
+ */
+public class NO814_BinaryTreePruning {
+
+}
