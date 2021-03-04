4eafe9d31ba17a2ec578f4ef15d3e63a2dc92ccc
mizhon <mizhon@live.cn>
2021-03-04 13:48:45 +0800

    添加试题:116 - 填充每个节点的下一个右侧节点指针

--- src/com/coolslow/playground/binary_tree/NO226_InvertBinaryTree.java
+++ src/com/coolslow/playground/binary_tree/NO226_InvertBinaryTree.java
@@ -20,7 +20,7 @@ package com.coolslow.playground.binary_tree;
  * 9   6  3   1
  */
 public class NO226_InvertBinaryTree {
-
+    // 官方写法
     public TreeNode invertBinaryTree(TreeNode root) {
         if (root == null) {
             return null;
@@ -30,6 +30,24 @@ public class NO226_InvertBinaryTree {
 
         root.left = right;
         root.right = left;
+
+        return root;
+    }
+
+    // 更直观的写法
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        // 不管在二叉树的哪一层，当前阶段需要做的就是交换其自身左右子树
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        // 递归调用左右子树
+        invertTree(root.left);
+        invertTree(root.right);
 
         return root;
     }
