c5183faf03b919717494ecf4dcc9c6a05f3ab616
mizhon <mizhon@live.cn>
2021-03-05 19:05:37 +0800

    添加699题：修剪一棵BST树

---
+++ src/com/coolslow/playground/binary_tree/NO699_TrimBinarySearchTree.java
@@ -0,0 +1,14 @@
+package com.coolslow.playground.binary_tree;
+
+/**
+ * 699. 修剪二叉搜索树
+ *
+ * 注意：
+ *  该题与814题有相似处，都是对二叉树进行修剪。但此题给定了low - high边界值。并且在修剪的过程中
+ *  树的节点位置可能发生变化。
+ *  区别，此题描述的是对 “二叉搜索树” 即：BST树 的修剪。
+ *
+ */
+public class NO699_TrimBinarySearchTree {
+
+}
