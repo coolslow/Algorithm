3ffa655eb6a8259da6765d20fb45db4a3383c729
mizhon <mizhon@live.cn>
2021-03-08 00:00:30 +0800

    110-添加先序遍历方法

--- src/com/coolslow/playground/binary_tree/NO110_BalancedBinaryTree.java
+++ src/com/coolslow/playground/binary_tree/NO110_BalancedBinaryTree.java
@@ -47,4 +47,19 @@ public class NO110_BalancedBinaryTree {
 
         return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
     }
+
+    public boolean isBalancedII(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return Math.abs(depth(root.left) - depth(root.right)) <= 1 && isBalanced(root.left) && isBalanced((root.right));
+    }
+
+    private int depth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return Math.max(depth(node.left), depth(node.right)) + 1;
+    }
 }
