195a9e0d80db3e57b7ba807030668fb9f810df0b
mizhon <mizhon@live.cn>
2021-03-16 23:49:16 +0800

    根据理解编写序列化二叉树的BFS解法

--- src/com/coolslow/playground/playground.java
+++ src/com/coolslow/playground/playground.java
@@ -1,4 +1,60 @@
 package com.coolslow.playground;
 
+//import com.coolslow.playground.binary_tree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class playground {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    /* =================================== **/
+    /* 序列化和反序列化二叉树                  **/
+    /* =================================== **/
+    public class LeetCode_NO297 {
+        // use [] to tag a tree node
+        // use "," to split nodes
+        // if node equals null -> null
+        // node.val -> String(node.val)
+        public String serialize(TreeNode root) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            while(!queue.isEmpty()) {
+                TreeNode node = queue.poll();
+                // 当节点为空时
+                if (node == null) {
+                    sb.append("null, ");
+                } else {
+                    sb.append(node.val + ",");
+                    queue.offer(node.left);
+                    queue.offer(node.right);
+                }
+            }
+
+            sb.append("]");
+
+            return sb.toString();
+        }
+
+//        public TreeNode deserialize(String data) {
+//
+//        }
+    }
+
 }
